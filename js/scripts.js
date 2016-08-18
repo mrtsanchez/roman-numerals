fc09e580a3b34ce869a55acc151ade37853b4f0b
Marta Sánchez and Sara Jensen <marta@>
2016-08-18 11:23:50 -0700

    Test the first spec for our roman numeral project

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
+  $("form#formInput").submit(function(event) {
+    event.preventDefault();
 
+    var arrayNumbers = [1, 5, 10, 50, 100, 500, 1000];
+    var arrayNumerals = ["I", "V", "X", "L", "C", "D", "M"];
+    var numberInput = parseInt($("#numberInput").val());
+
+    for (var i = 0; i < 1; i++) {
+      var indexOfNumber = arrayNumbers.indexOf(numberInput, 0);
+      var numeralOutput = arrayNumerals[indexOfNumber];
+      console.log(numeralOutput);
+    }
+    // var arrayNumbers = ["1", "5", "10", "50", "100", "500", "1000"];
+    // var arrayNumerals = ["I", "V", "X", "L", "C", "D", "M"];
+    // var numberInput = $("#numberInput").val();
+    // var numberInputArray = numberInput.split("");
+    //
+    // for (var i = 0; i < numberInputArray.length; i++) {
+    //   var indexOfNumber = arrayNumbers.indexOf(numberInput[i], 0);
+    //   var numeralOutput = arrayNumerals[indexOfNumber];
+    //   console.log(numeralOutput);
+    // }
+
+
+
+
+  });
 });
--- readme.md
+++ readme.md
@@ -1,15 +1,24 @@
-# Starter Pack
+# SRoman Numerals
 
-This is a starter pack for Epicodus Intro to Programming Projects.
+This is a method to convert numbers into Roman numerals.
 
-## Installation
+## Specs
 
-Clone the repository and use it to start your own Epicodus daily projects. 
+| Behavior | Input | Output |
+| --- | --- | --- |
+| If it's not a number | # | Not a number |
+| If the number is 0 | 0 | Not a roman number |
+| If the number is negative | - 2 | Not a roman number |
+| If the number is bigger than 3,999 | 5698 | Not a roman number |
+| If a single number matches one of the roman symbols, it should return the symbol | 5 | V |
+| If a tenth number matches one of the roman symbols, it should return the symbol | 5 | V |
+
+
 
 ## Credits
 
-Marta Sánchez
+Sara Jensen & Marta Sanchez
 
 This software is licensed under MIT license.
 
-Copyright (c) 2016 Marta Sanchez
+Copyright (c) 2016 Sara Jensen & Marta Sanchez
